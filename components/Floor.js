7f56847758fd1f973afadbfdc020c684d6e5b796
airiastmae <46886658+airiastmae@users.noreply.github.com>
2019-10-29 16:12:19 +0200

    bigger walls

--- components/Floor.js
+++ components/Floor.js
@@ -2,7 +2,7 @@ export default {
         template: `
         <a-entity class="floor" shadow="recieve: true">
             <a-plane rotation="-90 0 0" 
-            width="10" height="10" 
+            width="20" height="20" 
             color="#18181a" 
             material="src: ./assets/textures/TexturesCom_FloorsMedieval0037_1_S.jpg; repeat: 2 2">
             </a-plane>
--- index.js
+++ index.js
@@ -28,7 +28,8 @@ new Vue({
         <a-entity scale="0.5 0.5 0.5" position="0 0.5 0" class="flubber" obj-model="obj: #flubber; mtl: #flubber-mtl ">
         
         </a-entity>
-        <a-entity class="easel" obj-model="obj: #easel; mtl: #easel-mtl "></a-entity>
+        <!--<a-entity scale="5 5 5" class="easel" obj-model="obj: #easel; mtl: #easel-mtl "></a-entity> see ei t55ta-->
+        
 
         <Rig>
           <!-- Rig-i sisse võib panna asju, mis peaks liikuma koos kaameraga, märksõna HUD -->
@@ -36,9 +37,30 @@ new Vue({
           <a-text id="plakatitekst" value="Koopiamaal 'Affirmation II', akryyl" width="1" align="center" color="#FFF" visible="false" position="0 -0.05 -0.5" />
           <a-text id="primary" value="Natyyrmort, primaarid+valge akryyl" width="1" align="center" color="#FFF" visible="false" position="0 -0.05 -0.5" />
           <a-text id="sienna" value="Natyyrmort, ultramariin+burnt sienna" width="1" align="center" color="#FFF" visible="false" position="0 -0.05 -0.5" />
+          <a-text id="punane" value="Natyyrmort, monokroom punase akryyliga" width="1" align="center" color="#FFF" visible="false" position="0 -0.05 -0.5" />
+          <a-text id="sinine" value="Natyyrmort, monokroom sinise akryyliga" width="1" align="center" color="#FFF" visible="false" position="0 -0.05 -0.5" />
+          <a-text id="visand" value="Visand magavast vennast, akvarell" width="1" align="center" color="#FFF" visible="false" position="0 -0.05 -0.5" />
+          <a-text id="kuiv" value="Monokroom sinisega, kuiv akvarell" width="1" align="center" color="#FFF" visible="false" position="0 -0.05 -0.5" />
+          <a-text id="m2rg" value="Visand lilledest, m2rg akvarell" width="1" align="center" color="#FFF" visible="false" position="0 -0.05 -0.5" />
+          <a-text id="sega" value="Kompositsioon pea ja vaasiga, akvarell segatehnikas" width="1" align="center" color="#FFF" visible="false" position="0 -0.05 -0.5" />
+          <a-text id="pott" value="Visand lillepotist, pliiats" width="1" align="center" color="#FFF" visible="false" position="0 -0.05 -0.5" />
+          <a-text id="ymar" value="Joonistus ymaratest vormidest, pliiats" width="1" align="center" color="#FFF" visible="false" position="0 -0.05 -0.5" />
+          <a-text id="portr" value="Autoportree, visand pliiatsiga" width="1" align="center" color="#FFF" visible="false" position="0 -0.05 -0.5" />
+          <a-text id="vilt" value="Autoportree edasiarendus vildikaga" width="1" align="center" color="#FFF" visible="false" position="0 -0.05 -0.5" />
+          <a-text id="ini" value="Initsiaalid, tintenpen" width="1" align="center" color="#FFF" visible="false" position="0 -0.05 -0.5" />
+          <a-text id="tint" value="Joonistus, tintenpen" width="1" align="center" color="#FFF" visible="false" position="0 -0.05 -0.5" />
+          <a-text id="kuju" value="Vormi6petus, savifiguur" width="1" align="center" color="#FFF" visible="false" position="0 -0.05 -0.5" />
+          <a-text id="tont" value="V2rvi6petus, digimaal" width="1" align="center" color="#FFF" visible="false" position="0 -0.05 -0.5" />
+          <a-text id="apr" value="V2rvi6petus, digimaal" width="1" align="center" color="#FFF" visible="false" position="0 -0.05 -0.5" />
+          <a-text id="jim" value="V2rvi6petus, digimaal" width="1" align="center" color="#FFF" visible="false" position="0 -0.05 -0.5" />
+          <a-text id="semu" value="V2rvi6petus, digimaal" width="1" align="center" color="#FFF" visible="false" position="0 -0.05 -0.5" />
+          <a-text id="flint" value="V2rvi6petus, digimaal" width="1" align="center" color="#FFF" visible="false" position="0 -0.05 -0.5" />
+          <a-text id="hunt" value="V2rvi6petus, digimaal" width="1" align="center" color="#FFF" visible="false" position="0 -0.05 -0.5" />
+          <a-text id="reko" value="V2rvi6petus, digimaal" width="1" align="center" color="#FFF" visible="false" position="0 -0.05 -0.5" />
+          
         </Rig>
         
-        <Ceiling class="ceiling" position="0 4 0"/>
+        <Ceiling class="ceiling" position="0 4 4.85"/>
         <a-entity material="color: white; emissive: yellow; emissiveIntensity: 0.1" class="bulb" 
         position="0 3.42 -0.2" scale="2 2 2" obj-model="obj: #bulb" ></a-entity>
         <a-entity light="type: point; intensity: 0.4; color: #fffffa" position="0 2 0"></a-entity>
@@ -73,20 +95,184 @@ new Vue({
               event-set__leave="_event: mouseleave; _target: #sienna; visible: false"
 
           ></a-plane>
+          <a-plane 
+              class="hover" position="-5 0 0" scale="2 3 0"
+               material="src: ./assets/images/punasemonokroom.jpg"
+
+              event-set__enter="_event: mouseenter; _target: #punane; visible: true"
+              event-set__leave="_event: mouseleave; _target: #punane; visible: false"
+
+          ></a-plane>
+          <a-plane 
+              class="hover" position="-7.5 0 0" scale="2 3 0"
+               material="src: ./assets/images/sinisemonokroom.jpg"
+
+              event-set__enter="_event: mouseenter; _target: #sinine; visible: true"
+              event-set__leave="_event: mouseleave; _target: #sinine; visible: false"
+
+          ></a-plane>
+          <a-plane 
+              class="hover" position="5 0 0" scale="2 1.5 0"
+               material="src: ./assets/images/visand.jpg"
+
+              event-set__enter="_event: mouseenter; _target: #visand; visible: true"
+              event-set__leave="_event: mouseleave; _target: #visand; visible: false"
+
+          ></a-plane>
+          <a-plane 
+              class="hover" position="7.5 0 0" scale="2 3 0"
+               material="src: ./assets/images/kuivmonokroom.jpg"
+
+              event-set__enter="_event: mouseenter; _target: #kuiv; visible: true"
+              event-set__leave="_event: mouseleave; _target: #kuiv; visible: false"
+
+          ></a-plane>
           
         </Wall>
-      	<Wall position="-5 0 0" rotation="0 90 0">
+      	<Wall position="-10 0 4.985" rotation="0 90 0">
         <a-plane 
-              class="hover" position="-2.5 0 0" scale="2 3 0"
-               material="src: ./assets/images/ultramariinburntsienna.jpg"
+              class="hover" position="7 0 0" scale="2 2.5 0"
+               material="src: ./assets/images/initsiaal.jpg"
 
-              event-set__enter="_event: mouseenter; _target: #sienna; visible: true"
-              event-set__leave="_event: mouseleave; _target: #sienna; visible: false"
+              event-set__enter="_event: mouseenter; _target: #ini; visible: true"
+              event-set__leave="_event: mouseleave; _target: #ini; visible: false"
+
+          ></a-plane> 
+          <a-plane 
+              class="hover" position="4 0 0" scale="3 2 0"
+               material="src: ./assets/images/tintenpen.jpg"
+
+              event-set__enter="_event: mouseenter; _target: #tint; visible: true"
+              event-set__leave="_event: mouseleave; _target: #tint; visible: false"
+
+          ></a-plane> 
+          <a-plane 
+              class="hover" position="0.5 0 0" scale="3 2 0"
+               material="src: ./assets/images/kuju.jpg"
+
+              event-set__enter="_event: mouseenter; _target: #kuju; visible: true"
+              event-set__leave="_event: mouseleave; _target: #kuju; visible: false"
+
+          ></a-plane> 
+          <a-plane 
+              class="hover" position="-2.5 0 0" scale="2.5 2 0"
+               material="src: ./assets/images/1_halliskaala_tont.jpg"
+
+              event-set__enter="_event: mouseenter; _target: #tont; visible: true"
+              event-set__leave="_event: mouseleave; _target: #tont; visible: false"
+
+          ></a-plane> 
+          <a-plane 
+              class="hover" position="-5 0 0" scale="2 3 0"
+               material="src: ./assets/images/2_monokromaatiline_aprikoos.jpg"
+
+              event-set__enter="_event: mouseenter; _target: #apr; visible: true"
+              event-set__leave="_event: mouseleave; _target: #apr; visible: false"
+
+          ></a-plane> 
+          <a-plane 
+              class="hover" position="-7.5 0 0" scale="2 3 0"
+               material="src: ./assets/images/3_analoog_jimmy.jpg"
+
+              event-set__enter="_event: mouseenter; _target: #jim; visible: true"
+              event-set__leave="_event: mouseleave; _target: #jim; visible: false"
 
           ></a-plane> 
         </Wall>
-      	<Wall position="0 0 5" rotation="0 180 0"/>
-        <Wall position="5 0 0" rotation="0 -90 0"/>
+
+      	<Wall position="0 0 14.6" rotation="0 180 0">
+        <a-plane 
+              class="hover" position="7.5 0 0" scale="3 2 0"
+               material="src: ./assets/images/4_kolmikharmoonia_semu.jpg"
+
+              event-set__enter="_event: mouseenter; _target: #semu; visible: true"
+              event-set__leave="_event: mouseleave; _target: #semu; visible: false"
+
+          ></a-plane>
+          <a-plane 
+              class="hover" position="4 0 0" scale="2 3 0"
+               material="src: ./assets/images/5_vastandharmoonia_flint.jpg"
+
+              event-set__enter="_event: mouseenter; _target: #flint; visible: true"
+              event-set__leave="_event: mouseleave; _target: #flint; visible: false"
+
+          ></a-plane>
+          <a-plane 
+              class="hover" position="1 0 0" scale="3 2 0"
+               material="src: ./assets/images/6_jagunevadvastandid_hundu.jpg"
+
+              event-set__enter="_event: mouseenter; _target: #hunt; visible: true"
+              event-set__leave="_event: mouseleave; _target: #hunt; visible: false"
+
+          ></a-plane>
+          <a-plane 
+              class="hover" position="-2.5 0 0" scale="3 2 0"
+               material="src: ./assets/images/7_nelikharmoonia_rambo.jpg"
+
+              event-set__enter="_event: mouseenter; _target: #rambo; visible: true"
+              event-set__leave="_event: mouseleave; _target: #rambo; visible: false"
+
+          ></a-plane>
+          <a-plane 
+              class="hover" position="-5 0 0" scale="2 3 0"
+               material="src: ./assets/images/8_temperatuurikontrast_reko.jpg"
+
+              event-set__enter="_event: mouseenter; _target: #reko; visible: true"
+              event-set__leave="_event: mouseleave; _target: #reko; visible: false"
+
+          ></a-plane>
+        </Wall>
+
+        <Wall position="10 0 4.7" rotation="0 -90 0">
+        <a-plane 
+              class="hover" position="-7.5 0 0" scale="2 3 0"
+               material="src: ./assets/images/m2rgtehnika.jpg"
+
+              event-set__enter="_event: mouseenter; _target: #m2rg; visible: true"
+              event-set__leave="_event: mouseleave; _target: #m2rg; visible: false"
+
+          ></a-plane>
+          <a-plane 
+              class="hover" position="-5 0 0" scale="2 3 0"
+               material="src: ./assets/images/segatehnika.jpg"
+
+              event-set__enter="_event: mouseenter; _target: #sega; visible: true"
+              event-set__leave="_event: mouseleave; _target: #sega; visible: false"
+
+          ></a-plane>
+          <a-plane 
+              class="hover" position="-2 0 0" scale="2 3 0"
+               material="src: ./assets/images/potilill.jpg"
+
+              event-set__enter="_event: mouseenter; _target: #pott; visible: true"
+              event-set__leave="_event: mouseleave; _target: #pott; visible: false"
+
+          ></a-plane>
+          <a-plane 
+              class="hover" position="1 0 0" scale="3 2 0"
+               material="src: ./assets/images/ümaradvormid.jpg"
+
+              event-set__enter="_event: mouseenter; _target: #ymar; visible: true"
+              event-set__leave="_event: mouseleave; _target: #ymar; visible: false"
+
+          ></a-plane>
+          <a-plane 
+              class="hover" position="4 0 0" scale="2 3 0"
+               material="src: ./assets/images/pliiats.jpg"
+
+              event-set__enter="_event: mouseenter; _target: #portr; visible: true"
+              event-set__leave="_event: mouseleave; _target: #portr; visible: false"
+
+          ></a-plane>
+          <a-plane 
+              class="hover" position="7 0 0" scale="2 3 0"
+               material="src: ./assets/images/vildikas.jpg"
+
+              event-set__enter="_event: mouseenter; _target: #vilt; visible: true"
+              event-set__leave="_event: mouseleave; _target: #vilt; visible: false"
+
+          ></a-plane>
+        </Wall>
         <Box position="1 1 0" shadow="cast: true" 
         
         />
