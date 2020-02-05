cfc5283749bd68880ba45bae846637fec23664a8
airiastmae <46886658+airiastmae@users.noreply.github.com>
2020-02-05 22:18:12 +0200

    Update Wall.js

--- components/Wall.js
+++ components/Wall.js
@@ -2,7 +2,7 @@ export default {
         template: `
         <a-entity class="wall">
             <a-plane position="0 1.5 0" width="20" height="5" color="gray" 
-            material="src: ./assets/textures/TexturesCom_ConcreteLeaking0032_1_S.jpg; repeat: 1 1"> >
+            material="src: ./assets/textures/TexturesCom_ConcreteLeaking0032_1_S.jpg; repeat: 1 1">
             <a-entity position="0 0 0.01">
                     <!-- pilt läheb siia -->
                     <slot />
